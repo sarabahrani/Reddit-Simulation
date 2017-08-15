07c2af60007b5cebc03fdb3c33f063fb12316f69
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-08-14 17:51:46 -0700

    Apply css and bootstrap classes

--- src/components/reddit.js
+++ src/components/reddit.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Vote from './vote';
 class Reddit extends React.Component {
-render(){
-    const p=this.props.data;
-    return(
-        <div>
-            <Vote {...p}/>
-            <img src={p.thumbnail}/>
-            <h1><a href={p.url}>{p.title}</a></h1>
-            <p>by: {p.author}</p>
-            <p>{p.num_comments} comments</p>
-        </div>
-    );
-}
+    render() {
+        const p = this.props.data;
+        let thumbnail = {};
+        if (p.thumbnail == "self")
+            thumbnail = <img className="thumbnail self" />
+        else
+            thumbnail = <img src={p.thumbnail} />
+
+        return (
+            <div id="reddit">
+                <Vote {...p} />
+                <a className="thumbnail">
+                    {thumbnail}
+                </a>
+                <p className="link">
+                    <a href={p.url}>{p.title}</a>
+                </p>
+                <div className="tagline">
+                    <p className="domain">{p.domain}</p>
+                    <p className="author">by: {p.author}</p>
+                    <p className="comment" >{p.num_comments} comments</p>
+                </div>
+            </div>
+        );
+    }
 }
 export default Reddit;
